1257bceb9fcdbc9b4c15b9a2da8b3f0cbfc58ff1
Ashutosh <vashutosh625@gmail.com>
2024-03-15 01:18:57 +0530

    updated

--- script.js
+++ script.js
@@ -3,6 +3,8 @@
   const locomotiveScroll = new LocomotiveScroll();
 })();
 
+gsap.registerPlugin(ScrollTrigger);
+
 const logoMoveAnimation = () => {
   const logo = document.querySelector("#logo");
   logo.addEventListener("mousemove", (e) => {
@@ -22,39 +24,76 @@ const logoMoveAnimation = () => {
 
 logoMoveAnimation();
 
-const target = document.querySelectorAll("#page1-content h1");
+const homePageAnimations = () => {
+  const target = document.querySelectorAll("#page1-content h1");
+  let result1 = Splitting({ target: target[0] })[0].chars;
+  let result2 = Splitting({ target: target[1] })[0].chars;
+  const page1Timeline = gsap.timeline();
+  page1Timeline
+    .from("#page1-circle", {
+      scale: 0.5,
+      duration: 1,
+      ease: "power2.out",
+    })
 
-let result1 = Splitting({ target: target[0] })[0].chars;
-let result2 = Splitting({ target: target[1] })[0].chars;
+    .from(
+      [result1, result2],
+      {
+        y: "100%",
+        duration: 1.1,
+        ease: "power2.out",
+        stagger: 0.04,
+      },
+      ">-.5"
+    )
+    .from(
+      "#page1-content p",
+      {
+        y: 80,
+        opacity: 0,
+        duration: 2.1,
+        ease: "power2.out",
+      },
+      ">-.5"
+    )
+    .from(
+      "nav",
+      {
+        y: 100,
+        opacity: 0,
+        duration: 2.1,
+        ease: "power2.out",
+      },
+      ">-1.7"
+    );
+};
 
-const page1Timeline = gsap.timeline();
+homePageAnimations();
 
-page1Timeline
+const workPageAnimations = () => {
+  ScrollTrigger.create({
+    trigger: ".work:first-child",
+    start: "top 200px",
+    end: "=+300",
+    pin: true,
+    pinSpacing: false,
+  });
+};
 
-.from('#page1-circle', {
-    scale: .5,
-    duration: 1,
-    ease: "power2.out",
-})
+workPageAnimations();
 
-.from([result1, result2], {
-  y: "100%",
-  duration: 1.1,
-  ease: "power2.out",
-  stagger: 0.04,
-}, ">-.5")
-.from('#page1-content p', {
-   
-    y: 80,
-    opacity: 0,
-    duration: 2.1,
-    ease: "power2.out",  
-}, ">-.5")
-.from('nav', {
-    y: 100,
-    opacity: 0,
-    duration: 2.1,
-    ease: "power2.out",
-}, ">-1.7")
+const skillPageAnimations = () => {
 
+  // gsap.utils.toArray(".pin").forEach((element) => {
+  //   ScrollTrigger.create({
+  //     trigger: element,
+  //     start: "top 100px",
+  //     // end: '=+200',
+  //     pin: true,
+  //     pinSpacing: true,
+  //     marker: true,
+  //   });
+  // });
+};
 
+skillPageAnimations();
